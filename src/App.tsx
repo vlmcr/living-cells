781b846f9319107dff1bc20507f4c9e0ec517789
VadymDobrydnyk <vdobrydnyk@bluecrewjobs.com>
2021-04-15 14:20:56 +0300

    Init project, set animation loop, layout, random poinst

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const width = 50;
+const height = 50;
+
+const randomPointsCount = 10;
+
+const getRandomPoint = (): [number, number] => [
+  Math.floor(Math.random() * width),
+  Math.floor(Math.random() * height)
+]
+
+const fillArray = (length: number): number[] => {
+  const arr = [];
+  for (let i = 0; i < length; i++) {
+    arr[i] = 0
+  }
+  return arr;
+}
+
+const getRandomPoints = (): Array<[number, number]> => {
+  const arr: Array<[number, number]> = []
+  for (let i = 0; i < randomPointsCount; i++) {
+    arr.push(getRandomPoint())
+  }
+  return arr;
+}
+
+const fillArrayWithPoints = (points: Array<[number, number]>, array: number[]) => {
+  points.forEach(points => {
+    const x = points[0]
+    const y = points[1]
+    const index = (x * width) + y
+    array[index] = 1
+  })
+  return array;
+}
+
+
+
 function App() {
+  const randomPoints = getRandomPoints()
+  const emptyArr = fillArray(width * height)
+  const withLivePoints = fillArrayWithPoints(randomPoints, emptyArr)
+  const [filed, setField] = useState<number[]>(withLivePoints)
+
+  const delta = 10000;
+  let prevTime = Date.now();
+
+  const update = () => {
+    const time = Date.now();
+    if ((time - prevTime) > delta) {
+      prevTime = Date.now();
+
+
+    }
+
+    window.requestAnimationFrame(() => update());
+  }
+
+  useEffect(() => {
+    update()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {filed.map(point => <div className={point === 1 ? 'live' : 'died'} />)}
     </div>
   );
 }
